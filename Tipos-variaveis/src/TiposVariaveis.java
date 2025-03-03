744d22acbcb90a8b5fd88e1f6d63ce51ac827b2d
Caio <caiovfreitas@gmail.com>
2025-03-03 17:31:49 -0300

    Type char

--- Tipos-variaveis/src/TiposVariaveis.java
+++ Tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,10 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        char c1 = 65; // 'A'
+        char c2 = 66; // Algum caractere Unicode
+        System.out.println(c1);
+        System.out.println(c2);
     }
 }
