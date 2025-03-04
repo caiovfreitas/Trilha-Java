fc6108c0ea45005ea1eb14ae50e38d9e3739a890
Caio <caiovfreitas@gmail.com>
2025-03-03 23:21:08 -0300

    Inserindo possibilidade de input de usuário

--- Projeto-Smart-Tv/Projeto-Smart-TV/src/User.java
+++ Projeto-Smart-Tv/Projeto-Smart-TV/src/User.java
@@ -1,22 +1,70 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class User {
   public static void main(String[] args) throws Exception {
-    SmartTv smartTv = new SmartTv();
 
+    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+    SmartTv smartTv = new SmartTv();
     System.out.println(smartTv.ligada);
-    // smartTv.switchLigada();
-    // System.out.println(smartTv.ligada);
-    smartTv.aumentarVolume();
-    smartTv.switchLigada();
-    smartTv.aumentarVolume();
-    smartTv.aumentarVolume();
-    smartTv.aumentarVolume();
-    smartTv.diminuirVolume();
-    smartTv.subirCanal();
-    smartTv.subirCanal();
-    smartTv.subirCanal();
-    smartTv.mudarCanal(13);
-    smartTv.descerCanal();
-    smartTv.switchLigada();
-    smartTv.subirCanal();
+
+    while (!smartTv.ligada) {
+      System.out.println("Deseja ligar a TV? - S/N");
+      String ligarTv = scanner.next();
+      if (ligarTv.equalsIgnoreCase("S")) {
+        smartTv.switchLigada();
+        System.out.println("TV Ligada");
+      }
+    }
+
+    while (smartTv.ligada) {
+
+      System.out.println("--------------------------");
+      System.out.println("O que deseja realizar?");
+      System.out.println("1 - Aumentar volume");
+      System.out.println("2 - Diminuir volume");
+      System.out.println("3 - Subir canal");
+      System.out.println("4 - Descer canal");
+      System.out.println("5 - Inserir número do canal");
+      System.out.println("6 - Desligar TV");
+      System.out.println("--------------------------");
+
+      int escolhaUsuario = scanner.nextInt();
+
+      switch (escolhaUsuario) {
+        case 1:
+          smartTv.aumentarVolume();
+          break;
+
+        case 2:
+          smartTv.diminuirVolume();
+          break;
+
+        case 3:
+          smartTv.subirCanal();
+          break;
+
+        case 4:
+          smartTv.descerCanal();
+          break;
+
+        case 5:
+          System.out.println("Qual canal deseja assistir?");
+          int canal = scanner.nextInt();
+          smartTv.mudarCanal(canal);
+          break;
+
+        case 6:
+          smartTv.switchLigada();
+          System.out.println("Desligando...");
+          break;
+
+        default:
+          System.out.println("Opção inválida, tente novamente");
+          break;
+      }
+    }
+
+    scanner.close();
   }
 }
